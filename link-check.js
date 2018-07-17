e9729238bbbf2c5bd9261c2816d4c394df16cc36
Nick Schonning <Nick.Schonning@cic.gc.ca>
2018-07-16 21:16:04 -0400

    fix: Treat error links separately from Dead links

--- link-check.js
+++ link-check.js
@@ -7,34 +7,37 @@ var fs = require("fs");
 var glob = require("glob");
 var path = require("path");
 var chalk = require("chalk");
+var url = require("url");
 
-var files = glob.sync("**/*.md", {ignore: ["node_modules/**/*.md"]})
+var files = glob.sync("**/*.md", {
+  ignore: ["**/node_modules/**/*.md"],
+  realpath: true
+})
 
-var opts = JSON.parse(fs.readFileSync(".markdown-link-check.json"));
-opts.timeout = '30s'
+var config = JSON.parse(fs.readFileSync(".markdown-link-check.json"));
+config.timeout = '30s'
 
-files.forEach(function(file) {
+files.forEach(function (file) {
   var markdown = fs.readFileSync(file).toString();
+  let opts = Object.assign({}, config);
 
-  opts.baseUrl = path.dirname(path.resolve(file)) + '/';
+  opts.baseUrl = 'file://' + path.dirname(path.resolve(file));
 
   markdownLinkCheck(markdown, opts, function (err, results) {
     if (err) {
-        console.error('Error', err);
-        return;
+      console.error('Error', err);
+      return;
     }
 
     console.log(chalk.green("Reading: " + file));
 
     results.forEach(function (result) {
-      if(result.status === "dead") {
-        if (result.statusCode == 500) {
-          console.log(chalk.yellow("Server error on target: " + result.link));
-        }
-        else {
-          process.exitCode = 1
-          console.log(chalk.red("Dead: " + result.link));
-        }
+      if (result.status === "dead") {
+        process.exitCode = 1
+        console.log(chalk.red("Dead: " + result.link));
+      } else if (result.status == "error") {
+        process.exitCode = 1
+        console.log(chalk.red("Link Error: " + result.link));
       }
     });
   });
